4af8ffc6cdd22a3e1e7a8156ad6008d5442698e2
Zhangsan234 <2289011186@qq.com>
2020-08-07 17:27:44 +0800

    DFS

--- 100.相同树(Lv.1)/src/com/company/Main.java
+++ 100.相同树(Lv.1)/src/com/company/Main.java
@@ -37,6 +37,7 @@ package com.company;
   }
 
 class Solution {
+    {}
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null)return true;
         else if(p==null || q == null)return false;
