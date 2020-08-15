80ac89793fb3cd0fc7315bd3949bc9afef4fa56c
Zhangsan234 <2289011186@qq.com>
2020-08-15 14:34:29 +0800

    动态规划

---
+++ 546. 移除盒子（Lv.3）/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Solution solution = new Solution();
+        System.out.println(solution.removeBoxes(new int[]{1,3,2,2,2,3,4,3,1}));
+    }
+}
