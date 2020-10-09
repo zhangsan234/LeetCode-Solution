8dbc8d9fd8dc4159cb8235dd171ae431e8baa0c5
Zhangsan234 <2289011186@qq.com>
2020-10-09 11:38:23 +0800

    快慢指针

---
+++ 141. 环形链表（Lv.1）/src/com/company/Solution.java
@@ -0,0 +1,62 @@
+package com.company;
+
+/**
+ * @author Zhangsan
+ * @date 2020/10/9 11:29
+ */
+
+/**
+ * @problem
+ *给定一个链表，判断链表中是否有环。
+ *
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+ * 如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+ *
+ * 如果链表中存在环，则返回 true 。 否则，返回 false 。
+ * @example
+ *输入：head = [3,2,0,-4], pos = 1
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ *
+ *
+ * 输入：head = [1,2], pos = 0
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ *
+ *
+ * 输入：head = [1], pos = -1
+ * 输出：false
+ * 解释：链表中没有环。
+ *
+ * @idea
+ * 快慢指针
+ * 快指针每次移动两次 ， 慢指针每次移动一次 ，如果是环形链表就一定会出现两个指针指向同一个节点的情况。
+ * 要注意快指针的情况，快指针和他的下一个节点都要求不为空。
+ */
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null){
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(slow != fast){
+            if(fast == null || fast.next == null){
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
