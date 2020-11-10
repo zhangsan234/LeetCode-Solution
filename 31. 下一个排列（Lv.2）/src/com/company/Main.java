9dc7b27f300609c045b84a1a4d62a06f8dab9bc1
Zhangsan234 <2289011186@qq.com>
2020-11-10 22:47:43 +0800

    两边扫描

---
+++ 31. 下一个排列（Lv.2）/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Solution solution = new Solution();
+        int[] s = new int[]{4,3,2,1};
+        solution.nextPermutation(s);
+    }
+}
---
+++ 31. 下一个排列（Lv.2）/src/com/company/Solution.java
@@ -0,0 +1,59 @@
+package com.company;
+
+/**
+ * @author Zhangsan
+ * @date 2020/11/10 22:20
+ */
+
+/**
+ * @problem
+ * 实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+ *
+ * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+ *
+ * 必须原地修改，只允许使用额外常数空间。
+ *
+ * @example
+ *
+ * 以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
+ *
+ * @idea
+ * 先从数列最后遍历找到第一个降序的元素 nums[i]
+ * 在从数列最后遍历找到第一个大于 nums[i - 1] 的元素 nums[j]
+ * 将nums[i - 1] 和 nums[j]互换
+ * 再将 (i , n -1) 中的元素倒置
+ */
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public void reverse(int[] nums, int start) {
+        int left = start, right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+}
