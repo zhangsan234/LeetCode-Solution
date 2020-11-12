52915af8d1669bea8eff13a8addea9f356f9f1f3
Zhangsan234 <2289011186@qq.com>
2020-11-12 21:34:17 +0800

    分治

---
+++ 922. 按奇偶排序数组 II（Lv.1）/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Solution solution = new Solution();
+        int[] ints = solution.sortArrayByParityII(new int[]{});
+        for(int i : ints){
+            System.out.print(i + "   ");
+        }
+        System.out.println();
+    }
+}
